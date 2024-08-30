b3911e9a05a8e200ceeec2f089e6e6ae906983c7
noprizal <noprizal@voltras.co.id>
2024-08-30 19:01:23 +0700

    set resource service

---
+++ app/Filament/Resources/CarServiceResource.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CarServiceResource\Pages;
+use App\Filament\Resources\CarServiceResource\RelationManagers;
+use App\Models\CarService;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CarServiceResource extends Resource
+{
+    protected static ?string $model = CarService::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')->required(),
+                Forms\Components\TextInput::make('price')->required(),
+                Forms\Components\TextInput::make('about')->required(),
+                Forms\Components\FileUpload::make('photo')
+                    ->directory('photos')
+                    ->image()
+                    ->maxSize(1024) 
+                    ->acceptedFileTypes(['image/jpeg', 'image/png']),
+                Forms\Components\TextInput::make('duration_in_hour')->required(),
+            ]);
+    }
+    // 'name', 'price', 'about', 'photo', 'duration_in_hour', 'slug'
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')->searchable(),
+                Tables\Columns\TextColumn::make('price')->searchable(),
+                Tables\Columns\TextColumn::make('about')->searchable(),
+                Tables\Columns\TextColumn::make('duration_in_hour')->searchable(),
+                Tables\Columns\TextColumn::make('slug')->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCarServices::route('/'),
+            'create' => Pages\CreateCarService::route('/create'),
+            'edit' => Pages\EditCarService::route('/{record}/edit'),
+        ];
+    }
+}
